1bdd3613cfe15675d16a685734b4df97e7961fbf
JFrederic <frederic-jouan97440@outlook.fr>
2017-02-06 12:18:51 +0400

    add features

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const fs = require('fs')
+const path = require('path')
 
 
 function displayFiles(){
-    fs.readdir('./tmp', function (err, files) {
+    fs.readdir('./', function (err, files) {
     if (err) {
         throw err;
     }
-    console.log(files)
-    document.getElementById('files').innerHTML = files
+    for(let file of files){
+        let stats = fs.statSync(file)
+        let filePath = path.extname(file)
+        let fileSize = stats['size'] 
+        document.getElementById('files').innerHTML += '<tr><td>' + file + '</td><td>' + filePath + '</td><td>' +  fileSize + ' ko' + '</td></tr>' 
+        
+    }
 })
 }
 
